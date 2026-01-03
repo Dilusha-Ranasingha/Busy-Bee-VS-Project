3d97e672bed661ec8069492933002742bb6382a3
VeenathT <veenaththamuditha@gmail.com>
2026-01-04 02:50:25 +0530

    add db.py for database connection setup using psycopg2

---
+++ packages/ml-service/app/db.py
@@ -0,0 +1,16 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_conn():
+    return psycopg2.connect(
+        host=os.getenv("POSTGRES_HOST"),
+        port=os.getenv("POSTGRES_PORT"),
+        dbname=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        cursor_factory=RealDictCursor,
+    )
